e4ebd256188f9b40b1585882a98d47d516f52a22
Mikey <90205039+DurgaSaiRaj@users.noreply.github.com>
2026-03-19 20:41:39 +0530

    Time: 6 ms (18.73%), Space: 50.5 MB (10.48%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        int cur = 1;
+        while (res.size() < n) {
+            res.add(cur);
+            if (cur * 10 <= n) {
+                cur *= 10;
+            } else {
+                if (cur >= n) cur /= 10;
+                cur++;
+                while (cur % 10 == 0) cur /= 10;
+            }
+        }
+        return res;
+    }
+}
