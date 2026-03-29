3db6ed9017bec92f1ac17560eb0cbc2fcf992878
Mikey <90205039+DurgaSaiRaj@users.noreply.github.com>
2026-03-29 15:42:11 +0530

    Time: 3 ms (9.8%), Space: 9 MB (38.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mapping = {{')','('}, {'}','{'}, {']','['}};
+        
+        for (char c : s) {
+            if (mapping.count(c)) {
+                if (st.empty() || st.top() != mapping[c])
+                    return false;
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
